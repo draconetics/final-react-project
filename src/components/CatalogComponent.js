35e433d04db3eafe65a4ccd34b5c420ec2c0b793
Edson Terceros <edson.terceros@avantica.net>
2019-01-17 19:38:38 -0400

    mover items a un archivo externo usado props para flujo de datos padre hijo

--- src/components/CatalogComponent.js
+++ src/components/CatalogComponent.js
@@ -5,51 +5,10 @@ class Catalog extends Component {
 
     constructor(props, context) {
         super(props, context);
-        this.state = {
-            items: [
-                {
-                    id: 0,
-                    name: 'Helmet',
-                    image: '/assets/images/helmet.jpg',
-                    category: 'safety',
-                    label: 'ultra safe',
-                    price: '4.99',
-                    description: 'Un casco de seguridad útil para proteger en ámbitos como la construcción, fabricas y otros'
-                },
-                {
-                    id: 1,
-                    name: 'Audio protector',
-                    image: '/assets/images/audio.jpg',
-                    category: 'safety',
-                    label: 'noise filter',
-                    price: '12.99',
-                    description: 'Protege de ruidos auditivos por encima de los decibeles considerados aceptables'
-                },
-                {
-                    id: 2,
-                    name: 'Glasses',
-                    image: '/assets/images/glasses.jpg',
-                    category: 'safety',
-                    label: 'New technology',
-                    price: '31.99',
-                    description: 'Permite filtrar rayos ultravioleta, material anti rayas, anti caidas'
-                },
-                {
-                    id: 3,
-                    name: 'Gloves',
-                    image: '/assets/images/gloves.jpg',
-                    category: 'safety',
-                    label: '',
-                    price: '12.99',
-                    description: 'Hechos de algodón con goma antideslizante, especial para industria química.'
-                }
-
-            ]
-        }
     }
 
     render() {
-        let catalog = this.state.items.map(item => {
+        let catalog = this.props.items.map(item => {
             const imgStyle = {
                 maxHeight: 128,
                 maxWidth: 128

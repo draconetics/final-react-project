c69fb61dbc8c816912e3df6f792cb9e40b4f760d
Edson Terceros <edson.terceros@avantica.net>
2019-01-17 20:16:47 -0400

    eventos onClick funciones

--- src/components/CatalogComponent.js
+++ src/components/CatalogComponent.js
@@ -1,17 +1,40 @@
 import React, {Component} from 'react';
-import {Card, CardImg, CardImgOverlay, CardTitle} from "reactstrap";
+import {Card, CardBody, CardImg, CardImgOverlay, CardText, CardTitle} from "reactstrap";
 
 class Catalog extends Component {
 
     constructor(props, context) {
         super(props, context);
+        this.state = {
+            selectedItem: null
+        }
+    }
+
+    renderItem(item) {
+        if (item != null) {
+            return (
+                <Card>
+                    <CardImg width="100%" src={item.image} alt={item.name}/>
+                    <CardBody>
+                        <CardTitle>{item.name}</CardTitle>
+                        <CardText>{item.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        } else {
+            return (
+                <div/>
+            )
+        }
     }
 
     render() {
         let catalog = this.props.items.map(item => {
             return (
                 <div key={item.id} className="col-md-5 col-12  mt-5">
-                    <Card>
+                    <Card onClick={() => {
+                        this.onItemSelect(item)
+                    }}>
                         <CardImg width="100%" src={item.image} alt={item.name}/>
                         <CardImgOverlay>
                             <CardTitle>{item.name}</CardTitle>
@@ -23,11 +46,22 @@ class Catalog extends Component {
         return (
             <div className="container">
                 <div className="row">
-                        {catalog}
+                    {catalog}
+                </div>
+                <div className="row">
+                    {this.renderItem(this.state.selectedItem)}
                 </div>
 
             </div>
         );
+    }
+
+    onItemSelect(item) {
+        this.setState(
+            {
+                selectedItem: item
+            }
+        )
     }
 }
 

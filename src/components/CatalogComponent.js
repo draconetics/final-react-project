b91341fe484f811200e777ecc6276eaa69dad6e0
Edson Terceros <edson.terceros@avantica.net>
2019-01-22 07:26:35 -0400

    Breadcrumb and put catalog item into a link

--- src/components/CatalogComponent.js
+++ src/components/CatalogComponent.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import {Card, CardImg, CardImgOverlay, CardTitle} from "reactstrap";
+import {Breadcrumb, BreadcrumbItem, Card, CardImg, CardImgOverlay, CardTitle} from "reactstrap";
+import {Link} from "react-router-dom";
 
 function RenderCatalogItem({item, onClick}) {
     return (
-        <Card onClick={() => {
-            onClick(item.id)
-        }}>
-            <CardImg width="100%" src={item.image} alt={item.name}/>
-            <CardImgOverlay>
-                <CardTitle>{item.name}</CardTitle>
-            </CardImgOverlay>
-        </Card>
+        <Link to={`/catalog/${item.id}`}>
+            <Card>
+                <CardImg width="100%" src={item.image} alt={item.name}/>
+                <CardImgOverlay>
+                    <CardTitle>{item.name}</CardTitle>
+                </CardImgOverlay>
+            </Card>
+        </Link>
     );
 }
 
@@ -18,12 +19,22 @@ const Catalog = (props) => {
     var catalog = props.items.map(item => {
         return (
             <div key={item.id} className="col-12 col-md-5 m-1">
-                <RenderCatalogItem item={item} onClick={props.onClick}/>
+                <RenderCatalogItem item={item}/>
             </div>
         );
     });
     return (
         <div className="container">
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>Catalog</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Catalog</h3>
+                    <hr/>
+                </div>
+            </div>
             <div className="row">
                 {catalog}
             </div>
